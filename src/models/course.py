2b62e41c8654dc3de7355a61beba74c0095ce1fd
HomeroKemmerich <hmvkemmerich@gmail.com>
2025-02-13 15:10:42 -0300

    feat: refactor on `Course`
    - remove `Course.chapters` from `__init__()`;
    - add getter and setter methods for `Course.chapters`;

--- src/models/course.py
+++ src/models/course.py
@@ -1,5 +1,7 @@
 import json
 
+from functions import unformat
+
 from typing import List
 from models.chapter import Chapter
 
@@ -8,19 +10,20 @@ from markdown_utilities.pymd import dict_to_md
 
 class Course:
     title: str
+    stub: str
     description: str
     chapters: List[Chapter]
 
-    def __init__(self, title: str, description: str, chapters: List[Chapter]):
+    def __init__(self, title: str, description: str):
         self.title = title
+        self.stub = unformat(title.lower().replace(':', '').replace(' ', '-'))
         self.description = description
-        self.chapters = chapters
     
     def to_dict(self):
         return {
             'title': self.title,
             'description': self.description,
-            'chapters': [chapter.to_dict() for chapter in self.chapters]
+            'chapters': [chapter.to_dict() for chapter in self.get_chapters()]
         }
 
     def to_json(self):
@@ -39,3 +42,10 @@ class Course:
                     md_notes.append(md_blocks.h4(f'[{note.timestamp}]({note.url})'))
                     md_notes.append(md_blocks.p(f'{note.text}'))
         return dict_to_md(md_notes)
+
+    def get_chapters(self):
+        return self.chapters
+    
+    def set_chapters(self, chapters):
+        self.chapters = chapters
+        return self
