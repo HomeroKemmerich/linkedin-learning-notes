424c6357bb5298eabbeeaf059889d2bec33d18d6
HomeroKemmerich <hmvkemmerich@gmail.com>
2025-02-13 15:08:21 -0300

    feat: video refactor
    - remove `Video.notes` instatiation;
    - add getter and setter for `Video.notes`;

--- src/models/video.py
+++ src/models/video.py
@@ -1,16 +1,25 @@
 from typing import List
+from functions import unformat
 from models.note import Note
 
 class Video:
     title: str
+    stub: str
     notes: List[Note]
 
-    def __init__(self, title: str, notes: List[Note]):
+    def __init__(self, title: str):
         self.title = title
-        self.notes = notes
+        self.stub = unformat(title.lower().replace(' ', '-'))
 
     def to_dict(self):
         return {
             'title': self.title,
             'notes': [note.to_dict() for note in self.notes]
         }
+    
+    def get_notes(self):
+        return self.notes
+    
+    def set_notes(self, notes):
+        self.notes = notes
+        return self
